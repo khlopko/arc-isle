000be3820571fd0959b6f61ef7c82577b8523048
kyrylokhlopko <khlopko@gmail.com>
2024-03-01 22:09:22 +0100

    Refine main parser interface a little

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -15,35 +15,32 @@ use crate::schema::{ImportError, Schema};
 
 use self::interfaces::InterfacesParser;
 
-pub fn parse(file_path: &str) -> Result<Schema, Box<dyn std::error::Error>> {
+pub fn parse(parent_path: &str) -> Result<Schema, Box<dyn std::error::Error>> {
+    let file_path = &(parent_path.to_string() + "/main.yaml");
     let yaml = read_yaml(file_path)?;
     let main = &yaml[0];
-
     let hosts_parser = HostsParser { main };
     let hosts = hosts_parser.parse()?;
-
     let versioning_parser = VersioningParser { main };
     let versioning = versioning_parser.parse()?;
-
     let mut known_types: HashSet<String> = HashSet::new();
     let main_types_hash = main["types"]
         .as_hash()
         .ok_or(ImportError::InvalidInputSource)?;
-    let types_imports = detect(&main_types_hash, "example");
+    let types_imports = detect(&main_types_hash, parent_path);
     let mut types_parser = TypesParser {
-        parent_path: "example",
+        parent_path,
         known_types: &mut known_types,
     };
     let mut types: Vec<_> = vec![];
     for import in types_imports {
         types.extend(types_parser.parse(import?)?);
     }
-
     let main_interfaces_hash = main["interfaces"]
         .as_hash()
         .ok_or(ImportError::InvalidInputSource)?;
-    let interfaces_imports = detect(&main_interfaces_hash, "example");
-    let interfaces_parser = InterfacesParser::new("example", &known_types, &types);
+    let interfaces_imports = detect(&main_interfaces_hash, parent_path);
+    let interfaces_parser = InterfacesParser::new(parent_path, &known_types, &types);
     let mut interfaces: Vec<_> = vec![];
     for import in interfaces_imports {
         interfaces.extend(interfaces_parser.parse(import?)?);
@@ -54,6 +51,5 @@ pub fn parse(file_path: &str) -> Result<Schema, Box<dyn std::error::Error>> {
         types,
         interfaces,
     };
-
     Ok(schema)
 }
